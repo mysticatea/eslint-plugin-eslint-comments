5be249c7479a61e40ccef0a4f9508a81ff499ae0
Toru Nagashima <star.ctor@gmail.com>
2017-10-28 21:49:24 +0900

    New: `no-restricted-disable` rule

---
+++ docs/rules/no-restricted-disable.md
@@ -0,0 +1,65 @@
+# disallows `eslint-disable` comments about specific rules (no-restricted-disable)
+
+This rule warns `eslint-disable` directive-comments if the comment disable specific rules.
+
+## Rule Details
+
+:-1: Examples of **incorrect** code for `["no-undef", "no-unused-vars"]`:
+
+```js
+/*eslint no-restricted-disable: ["error", "no-undef", "no-unused-vars"]*/
+
+/*eslint-disable no-undef */
+f()
+```
+
+```js
+/*eslint no-restricted-disable: ["error", "no-undef", "no-unused-vars"]*/
+
+f() //eslint-disable-line no-undef
+```
+
+```js
+/*eslint no-restricted-disable: ["error", "no-undef", "no-unused-vars"]*/
+
+f() //eslint-disable-line
+```
+
+:+1: Examples of **correct** code for this rule:
+
+```js
+/*eslint no-restricted-disable: ["error", "no-undef", "no-unused-vars"]*/
+
+f() //eslint-disable-line another-rule
+```
+
+## Options
+
+```json
+{
+    "no-restricted-disable": ["error", ...]
+}
+```
+
+This rule takes a list of strings, where each string is a glob pattern. For example:
+
+```json
+{
+    "no-restricted-disable": [
+        "error",
+        "no-undef",
+        "*semi*", // rules which includes `semi`. E.g. `semi`, `semi-style`, `no-extra-semi`, ...
+        "react/*" // rules of `eslint-plugin-react`
+    ]
+}
+```
+
+```json
+{
+    "no-restricted-disable": [
+        "error",
+        "*",          // any rules
+        "!no-console" // but except `no-console`.
+    ]
+}
+```
---
+++ tests/lib/rules/no-restricted-disable.js
@@ -0,0 +1,120 @@
+/**
+ * @author Toru Nagashima
+ * @copyright 2016 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict"
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const RuleTester = require("eslint").RuleTester
+const rule = require("../../../lib/rules/no-restricted-disable")
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const tester = new RuleTester()
+
+tester.run("no-restricted-disable", rule, {
+    valid: [
+        "/*eslint-disable*/",
+        "//eslint-disable-line",
+        "//eslint-disable-next-line",
+        {
+            code: "/*eslint-disable b*/",
+            options: ["a"],
+        },
+        {
+            code: "/*eslint-enable a*/",
+            options: ["a"],
+        },
+        {
+            code: "/*eslint-disable a*/",
+            options: ["*", "!a"],
+        },
+    ],
+    invalid: [
+        {
+            code: "/*eslint-disable a*/",
+            options: ["a"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "/*eslint-disable*/",
+            options: ["a"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-line a",
+            options: ["a"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-line",
+            options: ["a"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-next-line a",
+            options: ["a"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-next-line",
+            options: ["a"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+
+        {
+            code: "/*eslint-disable a, b, c*/",
+            options: ["*", "!b", "!c"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "/*eslint-disable*/",
+            options: ["*", "!b", "!c"],
+            errors: ["Disabling '*,!b,!c' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-line a, b, c",
+            options: ["*", "!b", "!c"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-line",
+            options: ["*", "!b", "!c"],
+            errors: ["Disabling '*,!b,!c' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-next-line a, b, c",
+            options: ["*", "!b", "!c"],
+            errors: ["Disabling 'a' is not allowed."],
+        },
+        {
+            code: "//eslint-disable-next-line",
+            options: ["*", "!b", "!c"],
+            errors: ["Disabling '*,!b,!c' is not allowed."],
+        },
+
+        {
+            code: "/*eslint-disable semi, no-extra-semi, semi-style, comma-style*/",
+            options: ["*semi*"],
+            errors: [
+                "Disabling 'semi' is not allowed.",
+                "Disabling 'no-extra-semi' is not allowed.",
+                "Disabling 'semi-style' is not allowed.",
+            ],
+        },
+        {
+            code: "/*eslint-disable a, b, react/a, react/b*/",
+            options: ["react/*"],
+            errors: [
+                "Disabling 'react/a' is not allowed.",
+                "Disabling 'react/b' is not allowed.",
+            ],
+        },
+    ],
+})
